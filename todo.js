222217c72556f96b1cce7e7cdf3b7236779d5f22
sjh0302 <jhson8932@gmail.com>
2020-07-03 00:56:36 +0900

    d
    d

---
+++ todo.js
@@ -0,0 +1,72 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function filterFn(toDo) {
+  return toDo.id === 1;
+}
+
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    //filter는 배열의 모든 아이템을 통해 함수를 실행하고 그리고 true인 아이템들만 가지고 새로운 배열을 만든다.
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // JSON.stringify는 자바스키립트 객체를 문자열로 바꿔준다.
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos); // JSON.stringify는 자바스크립트 문자를 객체로 바꿔준다.
+    parsedToDos.forEach(function (toDo) {
+      //forEach는 배열에 담겨있는 것들 각각에 한번씩 함수를 실행 시켜준다.
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
---
+++ weather.js
@@ -0,0 +1,56 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "7f550e6823ab777151b4c3e333ecf10e";
+const COORDS = "coords";
+
+function getWeather(at, ng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat={at}&lon={ng}&appid={API_KEY}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude: latitude, //자바스크립트에서 key와 value 값이 같으면 생략 가능.
+    longitude: longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
